5933364682f9f12f9c5ad502ab034f0618054a8c
Tim Kilian <mail@timkilian.pl>
2019-01-21 18:20:31 +0100

    Copy assets to dist

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy-assets';
 import { uglify } from 'rollup-plugin-uglify';
 
 
@@ -16,7 +17,12 @@ export default {
         resolve({ customResolveOptions: { moduleDirectory: 'node_modules' } }),
         babel({ exclude: 'node_modules/**' }),
         json(),
-        commonjs()
+        commonjs(),
+        copy({
+            assets: [
+                './assets'
+            ],
+        }),
     ],
     external: [],
 };
