6d34ff9b7d918e1300bb7e7987b47df0ec4ef4d0
Marc Richter <marc@richter.bz>
2019-07-15 10:42:57 +0200

    Fix export of shapes at origin coordinates

--- src/serializers/PnmlSerializer.js
+++ src/serializers/PnmlSerializer.js
@@ -112,7 +112,7 @@ export default class PnmlSerializer {
 
         const graphicsElement = doc.createElement('graphics');
 
-        if (element.x && element.y) {
+        if (element.hasOwnProperty('x') && element.hasOwnProperty('y')) {
             const positionElement = doc.createElement('position');
             positionElement.setAttribute('x', element.x + element.width / 2);
             positionElement.setAttribute('y', element.y + element.height / 2);
